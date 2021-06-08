31ee35b6a61578d878e891082bbb7d4a7a4252d1
Caio Cesar <caio.sfg@gmail.com>
2021-06-07 23:41:03 -0300

    Inserindo alguns registros pro esboço

--- pokedex-vue/pokevue/src/App.vue
+++ pokedex-vue/pokevue/src/App.vue
@@ -2,6 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <ul>
+      <li v-for="(todo, i ) in todos" :key="i">{{ todo }}</li>
+    </ul>
+
+    <ul>
+      <li v-for="(value, chave) in insigna" :key="chave">{{ value }}</li>
+    </ul>
+
+    <template v-if="pokebola.length">
+      <ul>
+        <li v-for="(poke, index) in filterPoke" :key="index">{{ poke }}</li>
+      </ul>
+    </template>
+
   </div>
 </template>
 
@@ -12,6 +27,29 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      todos : [
+        'Pikachu',
+        'Squirtle',
+        'Charizard'
+      ],
+      pokebola:[
+        { poke: 'pokeball', have : true},
+        { poke: 'greatball', have : false},
+        { poke: 'megaball', have : true}
+      ],
+      insigna: {
+        medal1 : 'Jotoh',
+        medal2  : 'RockLeague'
+      }
+    }
+  },
+  computed:{
+    filterPoke (){
+      return this.pokebola.filter(poke => !poke.have)
+    }
   }
 }
 </script>
